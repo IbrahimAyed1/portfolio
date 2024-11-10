d214b05370aef5cc3bd1ba9de20042873043e4c4
Ibrahim Ayed <ibrahim@sherzal.com>
2024-11-11 03:08:23 +0400

    add docs for deployment

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{d as a}from"./chunk-DHN2F2BY.mjs";import"./chunk-X7EYED7G.mjs";import"./chunk-PBN5BIBB.mjs";import"./chunk-E44LSF4W.mjs";export{a as default};
