bcdafa13dd0e6bd480c16eed63377ce3e7fe6e84
Ibrahim Ayed <ibrahim@sherzal.com>
2024-11-10 20:31:08 +0400

    settings reset

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ResumeComponent } from './main/resume/resume.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:ResumeComponent}
+];
--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,10 +1,12 @@
-import { Client, Account} from 'appwrite';
+import { Account, Client } from 'appwrite';
 
 export const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('<PROJECT_ID>'); // Replace with your project ID
+    .setProject('ibrahim-ayed-portfolio'); // Replace with your project ID
 
 export const account = new Account(client);
+// const user = await account.createJWT();
+
 export { ID } from 'appwrite';
