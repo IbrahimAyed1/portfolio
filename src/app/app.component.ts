5c2c7ca983ebcdca6c1eb8dc39bde97ae41c3647
Ibrahim Ayed <ibrahim@sherzal.com>
2024-11-11 01:02:14 +0400

    feat: main resume page styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,112 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { account, ID } from '../lib/appwrite';
 import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDrawer, MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule],
+  imports: [RouterOutlet,FormsModule,MatSidenavModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-   me = {
-    "name": "Ibrahim Ayed",
-    "location": "Cairo, Egypt",
-    "title": "Front-End Developer",
-    "experience": "3+ years",
-    "specializations": ["TypeScript", "JavaScript", "HTML", "CSS", "Sass", "Angular", "Git", "RxJs", "NgRx"],
-    "objective": "Seeking a Front-End Developer position to leverage technical expertise in fast-paced environments.",
-    "workExperience": [
-      {
-        "position": "Senior Front-End Developer",
-        "company": "Integral Solutions",
-        "location": "Muscat, Oman",
-        "startDate": "January 2023",
-        "responsibilities": [
-          "Designed and developed a next-generation government service platform.",
-          "Utilized Angular v16 and NgMaterial v16 for visual consistency.",
-          "Architected front-end for scalability, maintainability, and performance.",
-          "Integrated HERE Maps and secure payment solutions."
-        ]
-      },
-      {
-        "position": "Front-End Developer",
-        "company": "Sherzal",
-        "location": "Cairo, Egypt",
-        "startDate": "July 2021",
-        "endDate": "January 2023",
-        "responsibilities": [
-          "Developed B2B and B2C modules for large-scale tour operation companies.",
-          "Enhanced performance using Angular OnPush strategies.",
-          "Created a design system and integrated REST APIs.",
-          "Managed front-end Git branches."
-        ]
-      },
-      {
-        "position": ".Net Full-Stack Web Developer Trainee",
-        "institution": "ITI",
-        "location": "Giza, Egypt",
-        "duration": "April 2021 - July 2021",
-        "programDetails": [
-          "Intensive training in C#, OOP, JavaScript, Angular, T-SQL, and SQL DBMS.",
-          "Developed console applications, interactive web applications, and managed databases."
-        ]
-      }
-    ],
-    "education": {
-      "degree": "Bachelor of Information Technology",
-      "institution": "Misr University for Science and Technology",
-      "department": "Information Systems",
-      "GPA": "3.22",
-      "topicsCovered": [
-        "Database Management",
-        "Software Development",
-        "System Analysis and Design",
-        "Project Management",
-        "Data Analytics"
-      ]
-    },
-    "skills": {
-      "proficientIn": [
-        "TypeScript",
-        "JavaScript",
-        "HTML",
-        "CSS",
-        "Sass",
-        "Angular",
-        "Git",
-        "RxJs"
-      ],
-      "knowledgeableIn": ["NgRx", "Micro Front-End Architecture", "Design Patterns"],
-      "otherSkills": [
-        "Mobile-First Design",
-        "Responsive Design",
-        "Cross-Browser Testing and Debugging",
-        "Agile Development",
-        "Scrum"
-      ],
-      "certifications": [
-        "VMware Fundamental Cloud Design & Architecture",
-        "Huawei HCIA",
-        "Amazon AWS Practitioner"
-      ]
-    },
-    "interests": [
-      "Certified Open Water Diver",
-      "Sci-fi and fantasy movies and television",
-      "Aspiring chef",
-      "Exploring advancements in front-end web development"
-    ],
-    "website": "https://ibrahimayed.com"
-  }
+export class AppComponent implements OnInit{
+  @ViewChild('drawer') drawer!: any;
   
   title = 'portfolio';
   loggedInUser: any = null;
   email: string = '';
   password: string = '';
   name: string = '';
+
+
+  ngOnInit(): void {
+    this.toggleDrawer();
+  }
+  toggleDrawer() {
+    debugger
+    // this.drawer.toggle();
+  }
 
   async login(email: string, password: string) {
     await account.createEmailPasswordSession(email, password);
--- src/app/main/resume/resume.component.ts
+++ src/app/main/resume/resume.component.ts
@@ -4,24 +4,29 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatChipsModule } from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [CommonModule,MatCardModule,
+  imports: [
+    CommonModule,
+    MatCardModule,
     MatDividerModule,
     MatExpansionModule,
     MatChipsModule,
-],
+    MatIconModule
+  ],
   templateUrl: './resume.component.html',
-  styleUrl: './resume.component.scss'
+  styleUrl: './resume.component.scss',
 })
 export class ResumeComponent {
   resume = {
     name: 'Ibrahim Ayed',
     title: 'Front-End Developer',
     location: 'Cairo, Egypt',
-    objective: 'Seeking a Front-End Developer position to leverage technical expertise in fast-paced environments.',
+    objective:
+      'Experienced Front End Developer with 3 years of experience in Software Development. Highly skilled in TS,JS, HTML, CSS,Sass Angular, Git, RxJs, and knowledge of NgRx. Demonstrated ability to work independently and collaboratively in a fast-paced environment. Seeking a Front-End Developer position to utilize my technical knowledge and experience..',
     workExperience: [
       {
         position: 'Senior Front-End Developer',
@@ -32,8 +37,9 @@ export class ResumeComponent {
           'Designed and developed a next-generation government service platform.',
           'Utilized Angular v16 and NgMaterial v16 for visual consistency.',
           'Architected front-end for scalability, maintainability, and performance.',
-          'Integrated HERE Maps and secure payment solutions.'
-        ]
+          'Integrated HERE Maps and secure payment solutions.',
+          'Collaborated closely with the backend development team to ensure seamless integration of front-end components with APIs, optimizing data flow and application performance.',
+        ],
       },
       {
         position: 'Front-End Developer',
@@ -45,22 +51,54 @@ export class ResumeComponent {
           'Developed B2B and B2C modules for large-scale tour operation companies.',
           'Enhanced performance using Angular OnPush strategies.',
           'Created a design system and integrated REST APIs.',
-          'Managed front-end Git branches.'
-        ]
-      }
+          'Managed front-end Git branches.',
+          'Expertly managed Front End GIT branches, leveraging the power of AWS CodeCommit as the dedicated platform for this crucial task.',
+          'Throughout my career, I have actively participated in numerous sub-projects, consistently prioritizing the creation of seamless user experiences. By leveraging my skills and expertise, I have successfully enhanced usability and accessibility in each of these initiatives.',
+        ],
+      },
     ],
     education: {
       degree: 'Bachelor of Information Technology',
       institution: 'Misr University for Science and Technology',
       department: 'Information Systems',
-      GPA: '3.22'
+      GPA: '3.22',
     },
     skills: {
-      proficientIn: ['TypeScript', 'JavaScript', 'HTML', 'CSS', 'Sass', 'Angular', 'Git', 'RxJs']
-    }
+      proficientIn: [
+        'TypeScript',
+        'JavaScript',
+        'HTML',
+        'CSS',
+        'Sass',
+        'Angular',
+        'Git',
+        'RxJs',
+        'RESTAPIs'
+      ],
+    },
   };
-  constructor(){
-    
+  displayedName = '';
+  displayedTitle = '';
+  displayedObjective = '';
+  [key: string]: any; // Add this line
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.typewriterEffect(this.resume.name, 'displayedName', 100);
+    this.typewriterEffect(this.resume.title, 'displayedTitle', 100);
+    this.typewriterEffect(this.resume.objective, 'displayedObjective', 50);
+  }
+
+  typewriterEffect(text: string, target: string, speed: number): void {
+    let index = 0;
+    const interval = setInterval(() => {
+      this[target] += text.charAt(index);
+      index++;
+      if (index === text.length) {
+        clearInterval(interval);
+      }
+    }, speed);
   }
 
 }
